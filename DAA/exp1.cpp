540d5fa432f05c1ebcdf1aff5f7cc6baa08134ca
Bhavya Trivedi <bhavya.swastika2006@gmail.com>
2026-01-15 13:09:22 +0530

    exp1.cpp

--- DAA/exp1.cpp
+++ DAA/exp1.cpp
@@ -1,51 +1,56 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h>#include <bits/stdc++.h>
 using namespace std;
-using namespace std::chrono;
-
-void complexRec(int n) {
-    int count=0;
-    if (n <= 2) {
-       return;
-   }
-
-   int p = n;
-   while (p > 0) {
-       vector<int> temp(n);
-       for (int i = 0; i < n; i++) {
-           temp[i] = i ^ p;
-           
-       }
-       p >>= 1;
-       count++;
-   }
-
-   vector<int> small(n);
-   for (int i = 0; i < n; i++) {
-       small[i] = i * i;
-       count++;
-   }
-
-   if (n % 3 == 0) {
-       reverse(small.begin(), small.end());
-       count++;
-   } else {
-       reverse(small.begin(), small.end());
-       count++;
-   }
-
-   complexRec(n / 2);
-   complexRec(n / 2);
-   complexRec(n / 2);
-   cout<<count;
+using namespace chrono;
+long long operations = 0;
+int maxDepth = 0;
+void complexRec(int n, int depth)
+{
+    maxDepth = max(maxDepth, depth);
+    if (n <= 2)
+    {
+        operations++;
+        return;
+    }
+    int p = n;
+    while (p > 0)
+    {
+        vector<int> temp(n);
+        operations++;
+        for (int i = 0; i < n; i++)
+        {
+            temp[i = i] = i ^ p;
+            operations++;
+        }
+        p >>= 1;
+        operations++;
+    }
+    vector<int> small(n);
+    operations++;
+    for (int i = 0; i < n; i++)
+    {
+        small[i] = i * i;
+        operations++;
+    }
+    reverse(small.begin(), small.end());
+    operations += n;
+    complexRec(n / 2, depth + 1);
+    complexRec(n / 2, depth + 1);
+    complexRec(n / 2, depth + 1);
 }
-// recursive relation = 3T(n/2) + n*log(n) + n
-// Complexity = O(n ^ (log (base b)a))
 
-int main(){
+int main()
+{
+    int n;
+    cin >> n;
+    operations = 0;
+    maxDepth = 0;
     auto start = high_resolution_clock::now();
-    complexRec(20);
-        auto end =  high_resolution_clock::now();
-        auto duration = duration_cast<milliseconds>(end-start);
-        cout<<duration;
-
-}
+    complexRec(n, 1);
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+    cout<<"Operations"<<operations<<endl;
+    cout<<"depth"<<maxDepth<<endl;
+    cout<<"time"<<duration.count()<<endl;
+    return 0;
+    //T(n)=3T(n/2)+O(nlogn)+n
+    //CASE 1:time complexity using case 1 O(n^log2(3))
